11e98579d9fd06d124739d7ca0a8d479b3f54217
Daniel Sanchez <daniel.ceinos@gmail.com>
2017-05-29 19:39:15 +0200

    Text char draw

--- app/src/main/java/com/arstotzka/asciinterface/CustomSurfaceView.kt
+++ app/src/main/java/com/arstotzka/asciinterface/CustomSurfaceView.kt
@@ -15,8 +15,8 @@ class CustomSurfaceView : SurfaceView {
     private var map: Array<IntArray>? = null
     private var sizeW = 0
     private var sizeH = 0
-    private val numColumns = 40
-    private val numRows = 26
+    private val numColumns = 10
+    private val numRows = 8
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attr: AttributeSet) : super(context, attr)
@@ -28,22 +28,30 @@ class CustomSurfaceView : SurfaceView {
             sizeW = width / numColumns
             sizeH = height / numRows
         }
+
         val p1 = Paint()
-        p1.color = Color.RED
+        p1.color = Color.GREEN
+        p1.textSize = 230F
+        p1.textAlign = Paint.Align.CENTER
+
         val p2 = Paint()
-        p2.color = Color.BLUE
+        p2.color = Color.BLACK
+        val p3 = Paint()
+        p3.color = Color.DKGRAY
+
         var p = false
+
         if (map != null && holder.surface.isValid) {
             val canvas = holder.lockCanvas()
             for (i in 0..(map as Array<IntArray>).size - 1) {
                 for (j in 0..(map as Array<IntArray>)[i].size - 1) {
                     val rect = Rect(i * sizeW, j * sizeH, i * sizeW + sizeW, j * sizeH + sizeH)
                     if (p)
-                        canvas.drawRect(rect, p1)
-                    else
                         canvas.drawRect(rect, p2)
+                    else
+                        canvas.drawRect(rect, p3)
                     p = !p
-
+                    canvas.drawText("|", (i * sizeW+sizeW/2).toFloat(), (j * sizeH + sizeH -2).toFloat(), p1)
                 }
                 p = !p
             }
