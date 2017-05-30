68e5858792a5dbc27f350aee5576b513063fd976
Daniel Sanchez <daniel.ceinos@gmail.com>
2017-05-30 13:19:35 +0200

    Set child view into view

--- app/src/main/java/com/arstotzka/asciinterface/MainActivity.kt
+++ app/src/main/java/com/arstotzka/asciinterface/MainActivity.kt
@@ -8,7 +8,6 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/arstotzka/asciinterface/views/AsciiView.kt
+++ app/src/main/java/com/arstotzka/asciinterface/views/AsciiView.kt
@@ -8,6 +8,7 @@ class AsciiView {
     private var x: Int
     private var y: Int
     private var mtx: Array<CharArray>?
+    private var child: AsciiView? = null
 
     constructor(x: Int, y: Int) {
         this.x = x
@@ -16,5 +17,14 @@ class AsciiView {
         mtx = Array(x) { CharArray(y) }
     }
 
-    
+    fun setChild(child: AsciiView) {
+        this.child = child
+
+        var childMtx = child.mtx
+        for (i in 0..childMtx!!.size - 1) {
+            (0..childMtx[i].size - 1)
+                    .filter { childMtx[i][it] != ' ' }
+                    .forEach { mtx!![i][it] = childMtx[i][it] }
+        }
+    }
 }
